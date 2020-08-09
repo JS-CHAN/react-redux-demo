b22fb49bf41c88b6e997a892da31f4e4f13fb109
js-chan <2694205616@qq.com>
2020-08-09 20:50:21 +0800

    Router ---- add react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import TodoList from "./TodoList";
 import {Provider} from 'react-redux';
 import store from './store'
-
+import {BrowserRouter, Route} from 'react-router-dom'
+import Footer from './Footer'
 const App = (
     <Provider store={store}>
-        <TodoList/>
+        <div>
+            <TodoList/>
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={Footer}/>
+                    <Route path='/detail' excat render = {()=><h1>This is detail page.</h1>}/>
+                </div>
+            </BrowserRouter>
+        </div>
     </Provider>
 );
 
